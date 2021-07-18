6756e7f5d1527d92d435960841c761c31fe8b86a
Valentin LUCAS <lucas.valentin77@gmail.com>
2021-07-19 00:28:25 +0200

    Add Streetview image component

---
+++ src/components/GStreetViewImage.vue
@@ -0,0 +1,34 @@
+<template>
+  <img src="" />
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'GStreetViewImage',
+    props: {
+      address: String
+    },
+    methods: {
+
+    },
+    mounted() {
+      console.log(this.imageURL);
+    },
+    computed: {
+      ...mapState(['googleApiKey']),
+      imageURL() {
+        return 'https://maps.googleapis.com/maps/api/streetview?location='+this.parsedAddress+'&size=300x200&key='+this.googleApiKey;
+      },
+      parsedAddress() {
+        return this.address.split(' ').join('+');
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
